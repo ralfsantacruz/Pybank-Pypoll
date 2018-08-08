409883c36a273c5525a0259c5e6a4e5accfada50
ralfsantacruz <rsantac1@gmail.com>
2018-08-07 23:00:29 -0700

    added print statements

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import statistics
 filepath = os.path.join('budget_data.csv')
 
 row_count = 0
+net_revenue = 0
 max_difference = 0
 min_difference = 0
 
@@ -14,15 +15,16 @@ revenue = []
 with open(filepath, newline = '') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter = ',')    
     csv_header = next(csv_reader)
-
     for row in csv_reader:
         row_count +=1
+        net_revenue += int(row[1])
         dates.append(row[0])
         revenue.append(int(row[1]))
 
+
 difference = [revenue[i+1]-revenue[i] for i in range(len(revenue)-1)]
 
-avg_difference = statistics.mean(difference)
+avg_difference = round(statistics.mean(difference),2)
 
 del dates[0]
 
@@ -35,16 +37,15 @@ for i, j in zip(dates, difference):
         min_difference = j
         min_date = i
 
-print(f"{max_difference} and {max_date}")
-print(f"{min_difference} and {min_date}")
-print(avg_difference)
-print(row_count)
+
+print("Financial Analysis")
+print("------------------------------------")
+print(f"Total Months: {row_count}")
+print(f"Total: ${net_revenue}")
+print(f"Average Change: ${avg_difference}")
+print(f"Greatest Increase in Profits: ${max_difference} on {max_date}")
+print(f"Greatest Decrease in Profits: ${min_difference} on {min_date}")
 
 
 
-    
-# print(dates)
-# print(revenue)
-# print(difference)
-# print(row_count)
 
